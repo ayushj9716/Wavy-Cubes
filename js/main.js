ad332a606cb9192dc3a92d5105f2da3017f60c70
Ayush Jain <ayush.jain@bizbrolly.com>
2018-05-03 01:20:40 +0530

    Basic animation done. Need to made some creative decisions like lighting, materials, interpolation etc.

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let angle = 0;
+function setup () {
+	createCanvas(canvasWidth, canvasHeight, WEBGL);
+	maxD = dist(0, 0, height/2, height/2);
+	maxHeight = height;
+}
+
+function draw() {
+	background(backgroundColor);
+	ortho(-canvasWidth, canvasWidth, canvasHeight, -canvasHeight, 0, 2*height);
+	
+	rotateX(QUARTER_PI);
+	rotateY(QUARTER_PI);
+
+	let offset = 0;
+	for(var z = 0; z < height; z += boxSize){
+		for (var x = 0; x < height; x += boxSize) {
+			push();
+			let d = dist(x,z,height/2,height/2);
+			let offset = map(d, 0, maxD, -PI, PI);
+			let a = angle + offset;
+			let h = floor(map(Math.sin(a), -1, 1, minHeight, maxHeight));
+			translate(x-height/2, 0, z-height/2);
+			normalMaterial();
+			box(boxSize, h, boxSize);
+			pop();
+		}
+	}
+	angle -= 0.08;	
+}
---
+++ js/settings.js
@@ -0,0 +1,7 @@
+var canvasWidth = window.innerWidth;
+var canvasHeight = window.innerHeight;
+var backgroundColor = 0;
+var boxSize = 65;
+var maxD;
+var minHeight = 200;
+var maxHeight = 600;
